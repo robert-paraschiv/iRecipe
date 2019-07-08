355833f9968f17a831e04a289c5eb79048d14072
Robert Paraschiv <paraschivlongin@gmail.com>
2019-07-08 11:18:49 +0300

    revert to BottomNavBar

--- app/src/main/java/com/rokudoz/irecipe/MainActivity.java
+++ app/src/main/java/com/rokudoz/irecipe/MainActivity.java
@@ -1,210 +1,125 @@
 package com.rokudoz.irecipe;
 
 import androidx.annotation.NonNull;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.appcompat.app.AppCompatActivity;
-import de.hdodenhof.circleimageview.CircleImageView;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.material.navigation.NavigationView;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.QuerySnapshot;
-import com.google.firebase.storage.FirebaseStorage;
-import com.rokudoz.irecipe.Account.LoginActivity;
 import com.rokudoz.irecipe.Fragments.FavoritesFragment;
 import com.rokudoz.irecipe.Fragments.HomeFragment;
 import com.rokudoz.irecipe.Fragments.ProfileFragment;
 import com.rokudoz.irecipe.Fragments.SearchFragment;
-import com.rokudoz.irecipe.Models.User;
-import com.squareup.picasso.Picasso;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.Objects;
 
 import static androidx.fragment.app.FragmentManager.POP_BACK_STACK_INCLUSIVE;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-    private DrawerLayout drawer;
-    private CircleImageView mImage;
-    private TextView mUserName, mUserEmail;
+    private static final String FRAGMENT_HOME = "HomeFragment";
+    private static final String FRAGMENT_SEARCH = "SearchFragment";
+    private static final String FRAGMENT_FAVORITES = "FavoritesFragment";
+    private static final String FRAGMENT_PROFILE = "ProfileFragment";
 
-    private String userDocumentID = "";
-    private String userProfilePicUrl = "";
-
-    private FirebaseAuth.AuthStateListener mAuthListener;
-    private User mUser;
-    private FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private CollectionReference recipeRef = db.collection("Recipes");
-    private CollectionReference usersReference = db.collection("Users");
-    private FirebaseStorage mStorageRef;
-
+    BottomNavigationView bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setupFirebaseAuth();
+        bottomNav = findViewById(R.id.bottom_navigation);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //I added this if statement to keep the selected fragment when rotating the device
+//        if (savedInstanceState == null) {
+//            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container,
+//                    new HomeFragment()).commit();
+//        }
+        setBottomNavigationView();
+    }
 
-        drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        View headerView = navigationView.getHeaderView(0);
 
-        mImage = headerView.findViewById(R.id.drawer_profile_pic);
-        mUserName = headerView.findViewById(R.id.drawer_profile_name);
-        mUserEmail = headerView.findViewById(R.id.drawer_profile_email);
+    Deque<Integer> mStack = new ArrayDeque<>();
+    boolean isBackPressed = false;
 
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+    private void setBottomNavigationView() {
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.nav_home:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                                new HomeFragment()).commit();
-                        break;
+                        if (!isBackPressed) {
+                            pushFragmentIntoStack(R.id.nav_home);
+                        }
+                        isBackPressed = false;
+                        setFragment(HomeFragment.newInstance(), FRAGMENT_HOME);
+                        return true;
                     case R.id.nav_search:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                                new SearchFragment()).commit();
-                        break;
+                        if (!isBackPressed) {
+                            pushFragmentIntoStack(R.id.nav_search);
+                        }
+                        isBackPressed = false;
+                        setFragment(SearchFragment.newInstance(), FRAGMENT_SEARCH);
+                        return true;
                     case R.id.nav_favorites:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                                new FavoritesFragment()).commit();
-                        break;
-                    case R.id.nav_profile:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                                new ProfileFragment()).commit();
-                        break;
+                        if (!isBackPressed){
+                            pushFragmentIntoStack(R.id.nav_favorites);
+                        }
+                        isBackPressed=false;
+                        setFragment(FavoritesFragment.newInstance(),FRAGMENT_FAVORITES);
+                        return true;
+                    case R.id.nav_account:
+                        if (!isBackPressed) {
+                            pushFragmentIntoStack(R.id.nav_account);
+                        }
+                        isBackPressed = false;
+                        setFragment(ProfileFragment.newInstance(), FRAGMENT_PROFILE);
+                        return true;
+
+                    default:
+                        return false;
                 }
-                drawer.closeDrawer(GravityCompat.START);
-                return true;
             }
         });
-        if (savedInstanceState == null && FirebaseAuth.getInstance().getCurrentUser() != null) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                    new HomeFragment()).commit();
-            navigationView.setCheckedItem(R.id.nav_home);
-            Log.d(TAG, "onCreate: HOMEFRAGMENT Created");
-        }
-
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
-                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
+        bottomNav.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+                if (item.getItemId()==R.id.nav_home){
+                    setFragment(HomeFragment.newInstance(), FRAGMENT_HOME);
+                }
+            }
+        });
+        bottomNav.setSelectedItemId(R.id.nav_home);
+        pushFragmentIntoStack(R.id.nav_home);
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        FirebaseAuth.getInstance().addAuthStateListener(mAuthListener);
+    private void pushFragmentIntoStack(int id) {
+        if (mStack.size() < 3) {
+            mStack.push(id);
+        } else {
+            mStack.removeLast();
+            mStack.push(id);
+        }
     }
 
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        if (mAuthListener != null) {
-            FirebaseAuth.getInstance().removeAuthStateListener(mAuthListener);
-        }
+    private void setFragment(Fragment fragment, String tag) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment, tag);
+        transaction.commit();
     }
 
     @Override
     public void onBackPressed() {
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
+        if (mStack.size() > 1) {
+            isBackPressed = true;
+            mStack.pop();
+            bottomNav.setSelectedItemId(mStack.peek());
         } else {
             super.onBackPressed();
         }
-    }
-
-    /*
-       ----------------------------- Firebase setup ---------------------------------
-    */
-    private void setupFirebaseAuth() {
-        Log.d(TAG, "setupFirebaseAuth: started");
-
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    //check if email is verified
-                    if (user.isEmailVerified()) {
-                        //If use is authenticated, perform query
-                        getUserInfo();
-                    } else {
-                        Toast.makeText(MainActivity.this, "Email is not Verified\nCheck your Inbox", Toast.LENGTH_SHORT).show();
-                        FirebaseAuth.getInstance().signOut();
-                    }
-
-                } else {
-                    // User is signed out
-                    Log.d(TAG, "onAuthStateChanged: signed_out");
-                    Toast.makeText(MainActivity.this, "Not logged in", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                    startActivity(intent);
-                    finish();
-                }
-                // ...
-            }
-        };
-    }
-
-    private void getUserInfo() {
-        usersReference.whereEqualTo("user_id", FirebaseAuth.getInstance().getCurrentUser().getUid())
-                .addSnapshotListener(new EventListener<QuerySnapshot>() {
-                    @Override
-                    public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
-                        if (e == null) {
-                            User user = queryDocumentSnapshots.getDocuments().get(0).toObject(User.class);
-                            userDocumentID = queryDocumentSnapshots.getDocuments().get(0).getId();
-                            userProfilePicUrl = user.getUserProfilePicUrl();
-
-                            mUserName.setText(user.getName());
-                            mUserEmail.setText(FirebaseAuth.getInstance().getCurrentUser().getEmail());
-
-                            if (!userProfilePicUrl.equals("")) {
-                                Picasso.get()
-                                        .load(userProfilePicUrl)
-                                        .error(R.drawable.ic_home_black_24dp)
-                                        .fit()
-                                        .centerCrop()
-                                        .into(mImage);
-                            } else if (userProfilePicUrl.equals("")) {
-                                Picasso.get()
-                                        .load(R.drawable.ic_home_black_24dp)
-                                        .placeholder(R.drawable.ic_home_black_24dp)
-                                        .into(mImage);
-                                Toast.makeText(MainActivity.this, "empty", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    }
-                });
     }
 }
