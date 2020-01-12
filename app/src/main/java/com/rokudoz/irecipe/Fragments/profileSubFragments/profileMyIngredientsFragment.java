3968be279ef4014c0f1f635e26f8626a75cefea3
Robert Paraschiv <paraschivlongin@gmail.com>
2020-01-13 00:01:29 +0200

    profile ingredients by category + fixed duplicate bug

--- app/src/main/java/com/rokudoz/irecipe/Fragments/profileSubFragments/profileMyIngredientsFragment.java
+++ app/src/main/java/com/rokudoz/irecipe/Fragments/profileSubFragments/profileMyIngredientsFragment.java
@@ -2,11 +2,14 @@ package com.rokudoz.irecipe.Fragments.profileSubFragments;
 
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Typeface;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +19,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.checkbox.MaterialCheckBox;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
@@ -31,6 +35,7 @@ import com.rokudoz.irecipe.R;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
@@ -125,38 +130,84 @@ public class profileMyIngredientsFragment extends Fragment {
                             }
 
                             List<String> checkboxNames = new ArrayList<>();
+                            List<String> ingredientCategoryList = new ArrayList<>();
+
+                            //setup ingredient names so that they don't duplicate later
                             for (MaterialCheckBox materialCheckBox : ingredientCheckBoxList) {
                                 checkboxNames.add(materialCheckBox.getText().toString());
                             }
+                            //Setup categories
                             for (Ingredient ingredient : userIngredientList) {
-                                if (!checkboxNames.contains(ingredient.getName()))
-                                    addIngredientLayout(ingredient);
+                                if (!ingredientCategoryList.contains(ingredient.getCategory())) {
+                                    ingredientCategoryList.add(ingredient.getCategory());
+                                }
                             }
+                            Collections.sort(ingredientCategoryList);
+                            for (String category : ingredientCategoryList) {
+                                List<Ingredient> ingredientsByCategoryList = new ArrayList<>();
+                                for (Ingredient ingredient : userIngredientList) {
+                                    if (ingredient.getCategory().equals(category) && !checkboxNames.contains(ingredient.getName()))
+                                        ingredientsByCategoryList.add(ingredient);
+                                }
+                                addCategoryOfIngredientsLayout(ingredientsByCategoryList, category);
+                            }
+
                         }
                     }
                 });
     }
 
-    private void addIngredientLayout(final Ingredient ingredient) {
-        if (getActivity() != null) {
+    private void addCategoryOfIngredientsLayout(List<Ingredient> ingredientList, String categoryName) {
+        if (getActivity() != null && !ingredientList.isEmpty()) {
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            );
+            params.setMargins(0, 4, 0, 4);
 
             final LinearLayout linearLayout = new LinearLayout(getActivity());
             linearLayout.setOrientation(LinearLayout.VERTICAL);
-            ingredientsLinearLayout.addView(linearLayout);
+            linearLayout.setLayoutParams(params);
 
-            MaterialCheckBox materialCheckBox = new MaterialCheckBox(getActivity());
-            materialCheckBox.setText(ingredient.getName());
-            materialCheckBox.setChecked(ingredient.getOwned());
-            materialCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                @Override
-                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                    userIngredientList.get(userIngredientList.indexOf(ingredient)).setOwned(isChecked);
-                    usersReference.document(userDocId).collection("Ingredients").document(ingredient.getDocumentId()).set(ingredient);
-                }
-            });
-            linearLayout.addView(materialCheckBox);
-            ingredientCheckBoxList.add(materialCheckBox);
+            TextView categoryNameTextView = new TextView(getActivity());
+            categoryNameTextView.setText(categoryName);
+            categoryNameTextView.setTextSize(12);
+//            categoryNameTextView.setTypeface(Typeface.DEFAULT_BOLD);
+            categoryNameTextView.setTextAppearance(R.style.TextAppearance_MaterialComponents_Headline6);
+            categoryNameTextView.setPadding(convertDpToPixel(8),4,0,0);
+            linearLayout.addView(categoryNameTextView);
+
+            for (Ingredient ingredient : ingredientList) {
+                addIngredientLayout(ingredient, linearLayout);
+            }
+            ingredientsLinearLayout.addView(linearLayout);
         }
+    }
+
+    private void addIngredientLayout(final Ingredient ingredient, LinearLayout linearLayout) {
+
+        MaterialCheckBox materialCheckBox = new MaterialCheckBox(getActivity());
+        materialCheckBox.setText(ingredient.getName());
+        materialCheckBox.setChecked(ingredient.getOwned());
+        materialCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                userIngredientList.get(userIngredientList.indexOf(ingredient)).setOwned(isChecked);
+                usersReference.document(userDocId).collection("Ingredients").document(ingredient.getDocumentId()).set(ingredient);
+            }
+        });
+        linearLayout.addView(materialCheckBox);
+
+        ingredientCheckBoxList.add(materialCheckBox);
+
+    }
+
+    //This function to convert DPs to pixels
+    private int convertDpToPixel(float dp) {
+        DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
+        float px = dp * (metrics.densityDpi / 160f);
+        return Math.round(px);
     }
 
 }
