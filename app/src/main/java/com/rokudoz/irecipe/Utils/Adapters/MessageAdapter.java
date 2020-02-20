ebe535bcb875fbe8f346425c3862dd43a6fdde86
Robert Paraschiv <paraschivlongin@gmail.com>
2020-02-20 10:26:03 +0200

    added "yesterday 20:40 " to messages

--- app/src/main/java/com/rokudoz/irecipe/Utils/Adapters/MessageAdapter.java
+++ app/src/main/java/com/rokudoz/irecipe/Utils/Adapters/MessageAdapter.java
@@ -34,6 +34,12 @@ import java.util.Map;
 
 
 public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageViewHolder> {
+
+    private static final int SECOND_MILLIS = 1000;
+    private static final int MINUTE_MILLIS = 60 * SECOND_MILLIS;
+    private static final int HOUR_MILLIS = 60 * MINUTE_MILLIS;
+    private static final int DAY_MILLIS = 24 * HOUR_MILLIS;
+
     private List<Message> messageList = new ArrayList<>();
     private static final String TAG = "MessageAdapter";
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -126,17 +132,54 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageV
 
         if (currentItem.getTimestamp() != null) {
             Date date = currentItem.getTimestamp();
-            DateFormat dateFormat = new SimpleDateFormat("HH:mm, MMM d", Locale.getDefault());
+            DateFormat dateFormat = new SimpleDateFormat("HH:mm, dd MM YYYY", Locale.getDefault());
             long time = date.getTime();
+            String timeAgo = getTimeAgo(time);
             if (currentItem.getTimestamp() != null && !currentItem.getTimestamp().equals("")) {
-                TimeAgo timeAgo = new TimeAgo();
-                holder.tvMessageTimeStamp.setText(timeAgo.getTimeAgo(time));
+                DateFormat smallDateFormat = new SimpleDateFormat("HH:mm", Locale.getDefault());
+                String timeString = smallDateFormat.format(date);
+                Log.d(TAG, "onBindViewHolder: " + timeAgo);
+                if (timeAgo.equals("just now") || timeAgo.equals("a minute ago") || timeAgo.contains("hours ago") || timeAgo.equals("an hour ago")) {
+                    holder.tvMessageTimeStamp.setText(getTimeAgo(time));
+                } else if (timeAgo.equals("yesterday")) {
+
+                    holder.tvMessageTimeStamp.setText(String.format("yesterday, %s", timeString));
+                } else
+                    holder.tvMessageTimeStamp.setText(String.format("%s, %s", timeAgo, timeString));
 
             }
         }
     }
 
+    public String getTimeAgo(long time) {
+        if (time < 1000000000000L) {
+            // if timestamp given in seconds, convert to millis
+            time *= 1000;
+        }
 
+        long now = System.currentTimeMillis();
+        if (time > now || time <= 0) {
+            return null;
+        }
+
+        // TODO: localize
+        final long diff = now - time;
+        if (diff < MINUTE_MILLIS) {
+            return "just now";
+        } else if (diff < 2 * MINUTE_MILLIS) {
+            return "a minute ago";
+        } else if (diff < 50 * MINUTE_MILLIS) {
+            return diff / MINUTE_MILLIS + " minutes ago";
+        } else if (diff < 90 * MINUTE_MILLIS) {
+            return "an hour ago";
+        } else if (diff < 24 * HOUR_MILLIS) {
+            return diff / HOUR_MILLIS + " hours ago";
+        } else if (diff < 48 * HOUR_MILLIS) {
+            return "yesterday";
+        } else {
+            return diff / DAY_MILLIS + " days ago";
+        }
+    }
 
     @Override
     public int getItemCount() {
